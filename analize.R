d1ac3c6a94db49125a2b864c1300cfaa30d3dab2
Polina Sonina <sarun4ik7@yandex.ru>
2017-12-21 12:33:51 +0300

    Add files via upload

--- analize.R
+++ analize.R
@@ -2,22 +2,28 @@ library(readr)
 rm(df)
 df <- read.csv("C:/Neat/Study/конструкции/data.csv", sep=";", encoding="utf-8")
 
-#логистическая регрессия-------------------------------------
-
-fit <- glm(target~Literal+Question+Negation+VerbTalk,data=df,family=binomial)
-summary(fit) # display results
-
 #корреляция переменных---------------------------------------
 library('corrplot')
 sub_df = subset(df, select = -c(target) )
 corrmatr <- cor(sub_df)
-res1 <- cor.mtest(sub_df, conf.level = .99)
-corrplot(corrmatr, p.mat = res1$p, insig = "blank")
+corr_mtest <- cor.mtest(sub_df, conf.level = .99)
+corrplot(corrmatr, p.mat = corr_mtest$p, insig = "blank")
 
 #случайный лес-----------------------------------------------
 library("randomForest")
 require(randomForest)
-fit <- randomForest(factor(target)~., data=df, importance = TRUE)
-importance(fit)
-varImpPlot(fit,type=2)
+fit_forest <- randomForest(factor(target)~., data=df, importance = TRUE)
+importance(fit_forest)
+varImpPlot(fit_forest, type=2) #plot MeanDecreaseGini
 
+#логистическая регрессия-------------------------------------
+df_new <- df
+df_new[,'Literal']<- as.factor(data[,'Literal'])
+df_new[,'Question']<- as.factor(data[,'Question'])
+df_new[,'Negation']<- as.factor(data[,'Negation'])
+df_new[,'VerbTalk']<- as.factor(data[,'VerbTalk'])
+
+fit_logit <- glm(target ~ Literal+Question+Negation+VerbTalk, data=df_new, family=binomial)
+summary(fit_logit)
+
+logit_pred = predict(fit_logit, type='response')
